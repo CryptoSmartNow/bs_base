74130788a36f3e6a69be6241ae4bc4ad35421fbc
xpanvictor <xpanvictor@gmail.com>
2024-10-11 12:40:45 +0100

    build(deployment): base mainnet

--- test/Bitsave.t.sol
+++ test/Bitsave.t.sol
@@ -37,13 +37,4 @@ contract BitsaveTest is Test, BitsaveConfigs {
        bitsave.joinBitsave{value: 0.000000001 ether}();
     }
 
-    // function test_Increment() public {
-    //     counter.increment();
-    //     assertEq(counter.number(), 1);
-    // }
-
-    // function testFuzz_SetNumber(uint256 x) public {
-    //     counter.setNumber(x);
-    //     assertEq(counter.number(), x);
-    // }
 }
